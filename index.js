98f683e733cbb393ce47687929ce49af2aabc2dd
pemrouz <p3pedram@hotmail.com>
2016-02-27 14:04:02 +0000

    should work deeply

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var last = require('utilise.last')
+  , key = require('utilise.key')
   , str = require('utilise.str')
   , is = require('utilise.is')
 
@@ -14,27 +15,46 @@ module.exports = exports = function set(diff) {
 exports.commit = function commit(o, diff) {
   var log = o.log
 
-  if (log) 
-    log.push({ 
-      diff: diff
-    , value: act.imm[diff.type](o, last(log).value, diff.key, diff.value) 
-    })
+  if (log) log.push({ 
+    diff: diff
+  , value: act.imm[diff.type](last(log).value, diff.key.split('.'), diff.value) 
+  })
 
-  if (o.emit) 
-    o.emit('log', diff)
+  if (o.emit) o.emit('log', diff)
 
   return o
 }
 
+function leaf(o, k, v){
+  var path = k.split('.')
+    , tail = path.pop()
+    , body = key(path.join('.'))(o)
+
+  return { body: body, tail: tail }
+}
+
 var act = {
   raw: {
-    update: function(o, k, v) { return o[k] = v }
-  , remove: function(o, k, v) { return is.arr(o) ? o.splice(k, 1)    : delete o[k] }
-  , add   : function(o, k, v) { return is.arr(o) ? o.splice(k, 0, v) : o[k] = v }
+    add   : function(o, k, v) { var l = leaf(o, k); return is.arr(l.body) ? l.body.splice(l.tail, 0, v) : key(k, v)(o) }
+  , update: function(o, k, v) { return key(k, v)(o) }
+  , remove: function(o, k, v) { 
+      var l = leaf(o, k)
+      return is.arr(l.body) ? l.body.splice(l.tail, 1)
+           : l.body         ? delete l.body[l.tail]
+           : false 
+    }
   }
 , imm: {
-    update: function(d, o, k, v) { return o.setIn(k.split('.'), v) }
-  , remove: function(d, o, k, v) { return is.arr(d) ? o.splice(k, 1)    : o.remove(k) }
-  , add   : function(d, o, k, v) { return is.arr(d) ? o.splice(k, 0, v) : o.set(k, v) }
+    update: function(o, k, v) { return o.setIn(k, v) }
+  , remove: function(o, k, v) { return o.deleteIn(k) }
+  , add   : function(o, k, v) { 
+      var path = k.slice(0, -1)
+        , tail = k.slice(-1)
+        , last = o.getIn(path)
+
+      return last && last.splice 
+           ? o.setIn(path, last.splice(tail, 0, v))
+           : o.setIn(k, v)
+    }
   }
 }
--- test.js
+++ test.js
@@ -8,26 +8,35 @@ describe('set', function() {
 
   it('should set value - object - add', function() {
     expect(set({ key: 'foo', value: 'bar', type: 'add' })({})).to.eql({ foo: 'bar' })
+    expect(set({ key: 'bar.baz', value: 'boo', type: 'add' })({})).to.eql({ bar: { baz: 'boo' } })
   })
 
   it('should set value - object - update', function() {
     expect(set({ key: 'foo', value: 'baz', type: 'update' })({ foo: 'bar' })).to.eql({ foo: 'baz' })
+    expect(set({ key: 'bar.baz', value: 'boo', type: 'update' })({})).to.eql({ bar: { baz: 'boo' } })
   })
 
   it('should set value - object - remove', function() {
     expect(set({ key: 'foo', value: 'bar', type: 'remove' })({ foo: 'bar' })).to.eql({})
+    expect(set({ key: 'foo', value: 'bar', type: 'remove' })({})).to.eql({})
+    expect(set({ key: 'foo.bar', value: 'bar', type: 'remove' })({ foo: { bar: 5 }})).to.eql({ foo: {} })
   })
 
   it('should set value - array - add', function() {
-    expect(set({ key: '0', value: 'foo', type: 'add' })([])).to.eql(['foo'])
+    expect(set({ key: '1', value: 'foo', type: 'add' })([1,2,3])).to.eql([1, 'foo', 2, 3])
+    expect(set({ key: '1.1', value: 'foo', type: 'add' })([1,{},3])).to.eql([1, { 1: 'foo' }, 3])
+    expect(set({ key: '1.1', value: 'foo', type: 'add' })([1, [1,2,3], 1])).to.eql([1, [1, 'foo', 2, 3], 1])
   })
 
   it('should set value - array - update', function() {
-    expect(set({ key: '0', value: 'bar', type: 'update' })(['foo'])).to.eql(['bar'])
+    expect(set({ key: '1', value: 'bar', type: 'update' })([1, 'foo', 2])).to.eql([1, 'bar', 2])
+    expect(set({ key: '1.1', value: 'bar', type: 'update' })([1,{},3])).to.eql([1, { 1: 'bar' }, 3])
   })
 
   it('should set value - array - remove', function() {
     expect(set({ key: '0', value: 'foo', type: 'remove' })(['foo'])).to.eql([])
+    expect(set({ key: '1.1', value: 'foo', type: 'remove' })([1, [2, 3, 4], 1])).to.eql([1, [2, 4], 1])
+    expect(set({ key: '1.1.1.1', value: 'foo', type: 'remove' })([1, [2, {}, 4], 1])).to.eql([1, [2, {}, 4], 1])
   })
 
   it('should set - object', function(){
@@ -109,6 +118,90 @@ describe('set', function() {
       { key: '0', value: 'foo', type: 'add' }
     , { key: '0', value: 'bar', type: 'update' }
     , { key: '0', value: 'bar', type: 'remove' }
+    ])
+
+  })
+
+
+  it('should set - object - deep', function(){
+    var changes = []
+      , o = versioned({}).on('log', function(diff){ changes.push(diff) })
+
+    expect(o).to.eql({})
+    expect(o.log.length).to.eql(1) 
+    expect(last(o.log).diff).to.eql(undefined)
+    expect(last(o.log).value.toJS()).to.eql({})
+    expect(changes).to.eql([])
+
+    expect(set({ key: '1.1', value: false, type: 'add' })(o)).to.eql(o)
+    expect(o).to.eql({ 1: { 1: false } })
+    expect(o.log.length).to.eql(2)
+    expect(last(o.log).diff).to.eql({ key: '1.1', value: false, type: 'add' })
+    expect(last(o.log).value.toJS()).to.eql(o)
+    expect(changes).to.eql([
+      { key: '1.1', value: false, type: 'add' }
+    ])
+
+    expect(set({ key: '1.1', value: true, type: 'update' })(o)).to.eql(o)
+    expect(o).to.eql({ 1: { 1: true } })
+    expect(o.log.length).to.eql(3)
+    expect(last(o.log).diff).to.eql({ key: '1.1', value: true, type: 'update' })
+    expect(last(o.log).value.toJS()).to.eql(o)
+    expect(changes).to.eql([
+      { key: '1.1', value: false, type: 'add' }
+    , { key: '1.1', value: true, type: 'update' }
+    ])
+
+    expect(set({ key: '1.1', value: true, type: 'remove' })(o)).to.eql(o)
+    expect(o).to.eql({ 1: {}})
+    expect(o.log.length).to.eql(4)
+    expect(last(o.log).diff).to.eql({ key: '1.1', value: true, type: 'remove' })
+    expect(last(o.log).value.toJS()).to.eql(o)
+    expect(changes).to.eql([
+      { key: '1.1', value: false, type: 'add' }
+    , { key: '1.1', value: true, type: 'update' }
+    , { key: '1.1', value: true, type: 'remove' }
+    ])
+  })
+
+  it('should set - array - deep', function(){
+    var changes = []
+      , o = versioned([1,[3],2]).on('log', function(diff){ changes.push(diff) })
+
+    expect(o).to.eql([1,[3],2])
+    expect(o.log.length).to.eql(1) 
+    expect(last(o.log).diff).to.eql(undefined)
+    expect(last(o.log).value.toJS()).to.eql(o)
+    expect(changes).to.eql([])
+
+    expect(set({ key: '1.1', value: 4, type: 'add' })(o)).to.eql(o)
+    expect(o).to.eql([1,[3,4],2])
+    expect(o.log.length).to.eql(2)
+    expect(last(o.log).diff).to.eql({ key: '1.1', value: 4, type: 'add' })
+    expect(last(o.log).value.toJS()).to.eql(o)
+    expect(changes).to.eql([
+      { key: '1.1', value: 4, type: 'add' }
+    ])
+
+    expect(set({ key: '1.1', value: 5, type: 'update' })(o)).to.eql(o)
+    expect(o).to.eql([1,[3,5],2])
+    expect(o.log.length).to.eql(3)
+    expect(last(o.log).diff).to.eql({ key: '1.1', value: 5, type: 'update' })
+    expect(last(o.log).value.toJS()).to.eql(o)
+    expect(changes).to.eql([
+      { key: '1.1', value: 4, type: 'add' }
+    , { key: '1.1', value: 5, type: 'update' }
+    ])
+
+    expect(set({ key: '1.1', value: 5, type: 'remove' })(o)).to.eql(o)
+    expect(o).to.eql([1,[3],2])
+    expect(o.log.length).to.eql(4)
+    expect(last(o.log).diff).to.eql({ key: '1.1', value: 5, type: 'remove' })
+    expect(last(o.log).value.toJS()).to.eql(o)
+    expect(changes).to.eql([
+      { key: '1.1', value: 4, type: 'add' }
+    , { key: '1.1', value: 5, type: 'update' }
+    , { key: '1.1', value: 5, type: 'remove' }
     ])
 
   })
